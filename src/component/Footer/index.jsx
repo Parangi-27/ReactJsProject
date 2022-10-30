1b63400ce54f9c60cb306a72902be35b61b2e336
Parangi Rathod <82773925+Parangi-27@users.noreply.github.com>
2022-10-30 10:54:23 +0530

    Navbar and Footer added

---
+++ src/component/Footer/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./footer.css";
+function Footer(){
+
+    return(
+            <>
+            <div class="wrapper">
+                <center>
+                <small>&copy;2022 <strong>Manav & Parangi</strong>, All Rights Reserved</small>
+                <nav class="footer-nav">
+                    <a href="#">Back to Top</a>
+                    {/* <a href="#">Terms of Use</a>
+                    <a href="#">Privacy</a> */}
+                </nav>
+                </center>
+            </div>
+            </>
+    );
+}
+
+export default Footer;
