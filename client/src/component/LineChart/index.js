2c2cf891e840f381a087ec5333183aa35a7b8926
Parangi Rathod <82773925+Parangi-27@users.noreply.github.com>
2022-11-11 23:27:48 +0530

    card modified

--- client/src/component/LineChart/index.js
+++ client/src/component/LineChart/index.js
@@ -4,6 +4,8 @@ import { Line } from "react-chartjs-2";
 import styles from "./styles.module.css";
 import "chart.js/auto"; // ADD THIS
 
+
+
 function LineChart() {
   const ref = useRef();
   const data = {
@@ -12,32 +14,34 @@ function LineChart() {
       {
         label: "Sales for 2020 (M)",
         data: [3, 2, 2, 1, 5],
+        borderColor: ['blue'],
+        backgroundColor: ['black'],
+        pointBackgroundColor: ['red'],
+        pointBorderColor: ['violet']
       },
     ],
-  // }
-  // options: {
-  //   animations: {
-  //       tension: {
-  //           duration: 1200,
-  //           easing: 'linear',
-  //           from: 1,
-  //           to: 0,
-  //           loop: true,
-  //       }
-  //   };
-  //   scales: {
-  //       y: {
-  //           min: 0;
-  //           max: 100;
-  //       }
-  //   }
+    options: {
+      animations: {
+        tension: {
+          duration: 1000,
+          easing: 'linear',
+          from: 1,
+          to: 0,
+          loop: true
+        }
+      },
+      scales: {
+        y: { // defining min and max so hiding the dataset does not change scale range
+          min: 0,
+          max: 100
+        }
+      }
+    }
 };
   
   return (
     <>
-    <div className = {styles.chart}>
       <Line ref={ref} data={data} />
-      </div>
     </>
   );
 }
--- client/src/component/card/Card.jsx
+++ client/src/component/card/Card.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import avtra from '../assets/images/avtraperson.jpg'
+import money from "./money.png";
 import './card.css'
 
 const Card = props => {
    // console.log("maanavsvvvv")
    // console.log(props.item)
     return (
+        <>
         <div className="card">
             <div className="card__img">
-             {/* <img src={avtra} alt="" /> */}
+             <img src={money} alt="" />
             </div>
             <div className="card__title">
-                <p>{props.item.name}</p> <p>{props.item.amount}</p><p>{props.item.description}</p>
+            <p>{props.item.name}</p> <p>{props.item.amount}</p><p>{props.item.description}</p>
                 {/* <p>{props.item.title}</p> */}
             </div>
         </div>
+
+        </>
     )
 }
 
